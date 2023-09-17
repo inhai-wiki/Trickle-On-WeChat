d5e308fec4557ad528d51e99e2452829de7e19e4
nimbus <46588426+PromptExpert@users.noreply.github.com>
2023-09-17 22:26:27 +0800

    v0.1

---
+++ Trickle-On-WeChat/CONFIG.py
@@ -0,0 +1,22 @@
+# 群聊名称
+GROUPS = ['AGI LAB']
+
+# FetchAPI key
+# 在本项目中，我是用的是FetchAPI，使用的是GTP3.5模型，在下面可以填入你的FetchAPI的Key信息，或者您可以接入自己的ChatGPT服务或其他大模型API。
+# FetchAPI注册地址https://frostsnowjh.com/#/?share=10000817
+
+API_KEY = ‘您的FetchAPI Key’
+
+
+# AIGCaaS openApi key
+# AIGCaaS主要为本项目提供了OCR的接口能力以及图像识别的能力，费用也相对来说较低，注册地址：https://www.aigcaas.cn/home_v3/login?share=AEjMzBfmyA
+
+SECRET_ID = '您的AIGCaaS SECRET_ID'
+SECRET_KEY= '您的AIGCaaS SECRET_ID'
+
+
+# 图片存放的地址，自定义您的图片存放地址
+
+PICS_DIC = '/Users/joseph/Desktop/TuTu/PICS/'
+
+MAX_LINE = 10
